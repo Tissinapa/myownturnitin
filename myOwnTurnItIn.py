fe35c1a6c4263941e2c784fc461c00b433183740
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2025-01-23 18:07:13 +0200

    pdf saved to mongo

--- myOwnTurnItIn.py
+++ myOwnTurnItIn.py
@@ -9,28 +9,30 @@ app = Flask(__name__)
 # Mongodb set up
 client = MongoClient("mongodb://localhost:27017")
 db = client["pdf_database"]
-collection = db["pdfs"]
+collection = db["peedeefs"]
 
 def extract_text_from_pdf(pdf_file):
     try: 
+        # Take text from pdf_file
         with pdfplumber.open(pdf_file) as pdf:
             return "\\n".join(page.extract_text() for page in pdf.pages if page.extract_text())
     except Exception as err:
-        print(f"Error reading pdf {pdf_file}: {err}")
+        print(f"Error reading PDF: {err}")
         return 
     
 def save_pdf_to_db(pdf):
     try:
-        with open(pdf, "rb") as pdf_file:
-            pdf_data = pdf_file.read()
+        pdf_text = extract_text_from_pdf(pdf)
+        if pdf_text:
             pdf_name = os.path.basename(pdf)
-
             #save to mongo
             collection.insert_one({
                 "filename": pdf_name,
-                "file_data": pdf_data
+                "file_data": pdf_text
             })
             print(f"pdf {pdf_name} saved to mongo.")
+        else: 
+            print(f"Failed to extract text from {pdf}")
     except Exception as err:
         print(f"Error reading pdf {pdf}: {err}")
         return
@@ -47,4 +49,8 @@ def compare_pdfs():
 
 if __name__ == "__main__":
     print("testi")
+    pdf_file= "test_pdf.pdf"
+    save_pdf_to_db(pdf_file)
+    
+    
 
